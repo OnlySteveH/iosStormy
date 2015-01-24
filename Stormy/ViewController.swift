86db12fc78c3abb1d8930a5351095c2748dbc01a
Steve Hunter <stevehunter@Steves-MBP.home>
2015-01-24 10:29:28 -0800

    Adding JSON handling

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
+    
+    private let apiKey = "d0bbfb359e73a19d161686e885d73d68"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(apiKey)/")
+        
+        let forecastURL = NSURL(string: "52.438297,1.390112", relativeToURL: baseURL)
+        
+        let sharedSession = NSURLSession.sharedSession()
+        let downloadTask: NSURLSessionDownloadTask = sharedSession.downloadTaskWithURL(forecastURL!, completionHandler: { (location: NSURL!, response: NSURLResponse!, error: NSError!) -> Void in
+            
+            let dataObject = NSData(contentsOfURL: location)
+            let weatherDictionary: NSDictionary = NSJSONSerialization.JSONObjectWithData(dataObject!, options: nil, error: nil) as NSDictionary
+            
+        })
+        downloadTask.resume()
+        
     }
 
     override func didReceiveMemoryWarning() {
---
+++ WeatherForecastAPITest.playground/section-1.swift
@@ -0,0 +1,23 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+let apiKey = "d0bbfb359e73a19d161686e885d73d68"
+
+
+let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(apiKey)/")
+
+
+let forecastURL = NSURL(string: "52.438,1.390", relativeToURL: baseURL)
+forecastURL!
+
+
+let weatherData = NSData(contentsOfURL: forecastURL!, options: nil, error: nil)
+
+println(weatherData)
+
+
+
+
+
+
